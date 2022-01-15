228b972015147661a033caf5677c442397e5ea7e
unknown <mdabdulhalimcse@gmail.com>
2022-01-16 03:25:13 +0600

    Eureka registry is created

--- eureka-registry/src/main/java/com/abdulhalim/EurekaRegistryApplication.java
+++ eureka-registry/src/main/java/com/abdulhalim/EurekaRegistryApplication.java
@@ -2,14 +2,14 @@ package com.abdulhalim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class CourseServiceApplication {
+@EnableEurekaServer
+public class EurekaRegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CourseServiceApplication.class, args);
+        SpringApplication.run(EurekaRegistryApplication.class, args);
     }
 
 }
